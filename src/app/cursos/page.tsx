56057f3f6bb6c67f505457679bc9fdc531dbbf06
gustavo <szeszgustavo@gmail.com>
2024-04-19 16:31:28 -0300

    rotas updated

---
+++ src/app/cursos/page.tsx
@@ -0,0 +1,14 @@
+// pages/cursos/index.tsx
+
+import * as React from 'react';
+
+const CursosPage: React.FC = () => {
+  return (
+    <div>
+      <h1>Cursos Page</h1>
+      {/* Add your content here */}
+    </div>
+  );
+};
+
+export default CursosPage;
