36cf736ece08ca6aca21aa247b306d3f09876bc9
oDouglass <douglasluiz_855@hotmail.com>
2024-04-20 07:48:58 -0300

    Descricao dos cursos

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,20 +10,25 @@ export default function Home() {
       <Navbar />
       <div className={styles['gradient-container']}>
         <div className={styles['card-container']}>
-        <CardCourses title="React" description="Aprenda agora" 
+        <CardCourses title="Veja os planos" description="Mergulhe em conteúdo com a maior comunidade de tecnologia do país." 
         image={<img src='../images/curse.logo.png' alt='react'></img>} />
         </div>
       </div>
 
       <Grid container spacing={2} className={styles['div-display-couses']}>
-        <CardCourses title=".NET" description="Aprenda a ultima versão" 
+        <CardCourses title="Programação" description="Lógica, Python, PHP, Java, .NET" 
         image={<img src='../images/curse.logo.png' alt='react'></img>}/>
-        <CardCourses title="Elixir" description="short desc"
+        <CardCourses title="Devops" description="AWS, Azure, Docker, Linux"
         image={<img src='../images/curse.logo.png' alt='react'></img>} />
-        <CardCourses title="Haskell" description="short descp"
+        <CardCourses title="Mobile" description="React Native, Flutter, IOS, Android"
+        image={<img src='../images/curse.logo.png' alt='react'></img>}/>
+        <CardCourses title="Front-End" description="HTML, CSS, React, Angular, JavaScript"
+        image={<img src='../images/curse.logo.png' alt='react'></img>}/>
+        <CardCourses title="Data Science" description="Ciência de dados, BI, SQL, NoSql"
+        image={<img src='../images/curse.logo.png' alt='react'></img>}/>
+        <CardCourses title="UX e Design" description="Usabilidade e UX, Video e Motion, 3D"
         image={<img src='../images/curse.logo.png' alt='react'></img>}/>
       </Grid>
-
 
       <footer className={styles['gradient-container-footer']}>
 
