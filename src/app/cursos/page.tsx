f0fae9ae667d249f6d2e75b119c9984083e0159a
oDouglass <douglasluiz_855@hotmail.com>
2024-04-21 21:13:57 -0300

    Usando MUI para página sobre-nos

--- src/app/cursos/page.tsx
+++ src/app/cursos/page.tsx
@@ -1,12 +1,11 @@
-// pages/cursos/index.tsx
-
 import * as React from 'react';
 
 const CursosPage: React.FC = () => {
   return (
     <div>
+
       <h1>Cursos Page</h1>
-      {/* Add your content here */}
+      
     </div>
   );
 };
